d61945262bec763f683c174601505c364fb13b18
aljazsim <aljazsim@gmail.com>
2018-08-06 01:35:42 -0400

    - updated build script

--- Build/publishNuGetPackage.ps1
+++ Build/publishNuGetPackage.ps1
@@ -1,3 +1,5 @@
+param($nugetKey="")
+
 $ErrorPreference = "Stop"
 
 $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
@@ -14,4 +16,4 @@ $nugetPackageFilePath = "..\Source\TransformationFramework\bin\Release\Transform
 & "$msbuild" "$projectFilePath" /t:pack /p:Configuration=Release
 
 # publish package
-& "$nuget" push "$nugetPackageFilePath" xxx -Source https://api.nuget.org/v3/index.json
+& "$nuget" push "$nugetPackageFilePath" $nugetKey -Source https://api.nuget.org/v3/index.json
